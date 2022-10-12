d48c57f126461bb4371573d20f2ce52032f63921
Rani <109195584+ranisharma09@users.noreply.github.com>
2022-10-13 05:28:18 +0530

    Create BXB_FUCK.py

---
+++ BXB_FUCK.py
@@ -0,0 +1,6 @@
+import os,time
+os.system("xdg-open https://youtube.com/channel/UCbT--Z1XzQpSUgjD6bfzzUA")
+print("\n\n\t\033[1;32mWell come to bypass BXB")
+time.sleep(1)
+import RNDM
+RNDM.password()
